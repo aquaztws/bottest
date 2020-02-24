f526ba8ba955953b34eb57db389ed25903dc7ee8
aquaztws <59536437+aquaztws@users.noreply.github.com>
2020-02-25 00:38:28 +0100

    Add files via upload

---
+++ .gitignore/index.js
@@ -0,0 +1,27 @@
+const Discord = require("discord.js")
+const client = new Discord.client()
+let prefix = "!"
+
+module.exports = class toby extends command {
+	static  match(message) {
+
+
+		return message.content.startsWith('Suceur?')
+	}
+
+
+	static action (message){
+		 if (message.member.hasPermission("ADMINISTRATOR")){
+		 	message.channel.send("OUI MAITRE")
+	} else {
+		for (var i =0; i <= 5; i++) {
+
+			message.channel.send("TOBY N'EST PAS TON ELFE " + (message.reply("")))
+			message.channel.send("TOBY EN COLERE")
+		}
+	}
+
+
+}
+
+}
